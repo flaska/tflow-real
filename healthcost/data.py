34dfff571b20bf263207ab726e97513cac5e0e4b
Jakub Flaska <jakub.flaska@nih.gov>
2018-09-12 17:06:49 -0400

    x

--- healthcost/data.py
+++ healthcost/data.py
@@ -1,9 +1,33 @@
 import csv
-result = []
+all_data = []
+all_labels = []
+
+string_data = []
 with open('insurance.csv', 'rt') as csvfile:
      reader = csv.reader(csvfile, delimiter=',', quotechar='|')
      for row in reader:
-        result.append(row)
+         string_data.append(row)
+
+string_data = string_data[1:]
+
+for row in string_data:
+    new_row = [
+        float(row[0]),
+        float(row[2])
+    ]
+    all_data.append(new_row)
+
+for row in string_data:
+    new_row = [
+        float(row[6])
+    ]
+    all_labels.append(new_row)
+
+train_data = all_data[:1000]
+train_labels = all_labels[:1000]
+
+test_data = all_data[1000:]
+test_labels = all_labels[1000:]
 
 def get_data():
-    return result[1:]
+    return (train_data, train_labels), (test_data, test_labels)
--- healthcost/main.py
+++ healthcost/main.py
@@ -1,3 +1,5 @@
 import data
-data = data.get_data()
-print(data)
+
+(train_data, train_labels), (test_data, test_labels) = data.get_data()
+print(train_data)
+print(train_labels)
